80311d80f6e2388403179fc676aed475f25d1833
Syahrul ANDRI <syahrul_173040161@mail.unpas.ac.id>
2019-12-13 03:29:00 +0700

    konfigurasi pemanggilan assets pada file HEADER.PHP dan konfigurasi file DASHBOARD.PHP pada folder CONTROLLER

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -5,6 +5,10 @@
 		
 		public function index(){
 			
+
+			$this->load->view('templates/header');
+			$this->load->view('templates/sidebar');
 			$this->load->view('dashboard');
+			$this->load->view('templates/footer');
 		}
 	}
