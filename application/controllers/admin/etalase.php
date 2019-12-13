26fb6fda1f095e751f6545e35d7904daa9f208f9
Syahrul ANDRI <syahrul_173040161@mail.unpas.ac.id>
2019-12-14 01:58:48 +0700

    membuat file ETALASE.PHP controller etalase pada folder CONTROLLERS/ADMIN

---
+++ application/controllers/admin/etalase.php
@@ -0,0 +1,15 @@
+<?php  
+
+		
+		class etalase extends CI_Controller(){
+			public function index(){
+
+				$this->load->view('templates_admin/header');
+				$this->load->view('templates_admin/sidebar');
+				$this->load->view('admin/etalase');
+				$this->load->view('templates_admin/footer');
+			}
+
+		}
+
+
